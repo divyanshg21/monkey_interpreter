afa0f1860be3d23c482fcb8b8004af17fdd3b440
[Divyansh Gupta] <[divyanshgupta2003@gmail.com]>
2025-02-18 16:13:02 +0530

    Lexer_done

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -18,6 +18,14 @@ func New(input string) *Lexer {
 	return l
 }
 
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+}
+
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
@@ -34,18 +42,51 @@ func (l *Lexer) NextToken() token.Token {
 	l.skipWhitespace()
 
 	switch l.ch {
-	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
-	case ';':
+	case'=':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
+		} else{
+			tok = newToken(token.ASSIGN, l.ch)
+		}
+		
+	case'+':
+		tok = newToken(token.PLUS, l.ch)
+	case'-':
+		tok = newToken(token.MINUS, l.ch)
+	case'!':
+		if l.peekChar() == '=' {
+			ch:= l.ch
+			l.readChar()
+			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
+	case'/':
+		tok = newToken(token.SLASH, l.ch)
+	case'*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case'<':
+		tok = newToken(token.LT, l.ch)
+	case'>':
+		tok = newToken(token.GT, l.ch)
+	case';':
 		tok = newToken(token.SEMICOLON, l.ch)
+	case',':
+		tok = newToken(token.COMMA, l.ch)
+	// case '=':
+	// 	tok = newToken(token.ASSIGN, l.ch)
+	// case ';':
+	// 	tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
 		tok = newToken(token.LPAREN, l.ch)
 	case ')':
 		tok = newToken(token.RPAREN, l.ch)
-	case ',':
-		tok = newToken(token.COMMA, l.ch)
-	case '+':
-		tok = newToken(token.PLUS, l.ch)
+	// case ',':
+	// 	tok = newToken(token.COMMA, l.ch)
+	// case '+':
+	// 	tok = newToken(token.PLUS, l.ch)
 	case '{':
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
@@ -105,3 +146,4 @@ func isLetter(ch byte) bool {
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
+
