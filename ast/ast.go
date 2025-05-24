f85af84a0e6b56b50e5215d5bc922275f7b1c908
Divyansh Gupta <divyanshgupta2003@gmail.com>
2025-05-24 19:05:23 +0530

    added_parser

--- ast/ast.go
+++ ast/ast.go
@@ -27,3 +27,19 @@ func (p*Program) TokenLiteral() string {
 	}
 }
 
+type LetStatement struct {
+	Token token.Token
+	Name *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement)  statementNode()		{}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal}
+
+type Identifier struct	{
+	Token token.Token
+	value string
+}
+
+func (i *Identifier) expressionNode()		{}
+func (i *Identifier) TokenLiteral() string {return i.Token.Literal}
