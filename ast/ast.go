2ff0f106bef33b31f04e18fb647b20c55203044e
Divyansh Gupta <divyanshgupta2003@gmail.com>
2025-07-22 23:40:26 +0530

    import_error

--- ast/ast.go
+++ ast/ast.go
@@ -46,7 +46,18 @@ type ExpressionStatement struct{
 	Expression Expression
 }
 
+type IntegerLiteral struct{
+	Token token.Token
+	Value int64
+}
 
+
+
+func(il *IntegerLiteral) expressionNode()	{}
+
+func(il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal}
+
+func(il *IntegerLiteral) String() string 	{ return il.Token.Literal}
 
 func (es *ExpressionStatement) statementNode(){}
 
--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/divyanshg21/monkey_interpreter/ast"
 	"github.com/divyanshg21/monkey_interpreter/lexer"
@@ -44,6 +45,7 @@ func New(l*lexer.Lexer) *Parser{
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	p.nextToken()
 	p.nextToken()
@@ -173,3 +175,16 @@ func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn){
 	p.infixParseFns[tokenType]=fn
 }
 
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err!=nil {
+		msg := fmt.Sprint("could not parse %q as an integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+
+	return lit
+}
